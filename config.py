2f5798e8ecef5b52fc73535bb69fc98d472b35e6
Javier Gonel <bolibic@gmail.com>
2016-12-11 19:08:49 +0700

    Set the potato token with lower ban time

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 RATE_LIMITS = {
-    "potato": dict(requests=1, per_seconds=10, ban=30),
+    "potato": dict(requests=3, per_seconds=10, ban=20),
     "superkey": dict(requests=1, per_seconds=5, ban=40)
 }
